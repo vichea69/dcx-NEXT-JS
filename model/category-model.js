869c4f45b34ee7dd317b7fc3720465ea542a48a2
Vichea69 <smart.vichea@gmail.com>
2025-07-18 16:17:18 +0700

    Refactor category and user models; implement mongoose schemas and update course query

--- model/category-model.js
+++ model/category-model.js
@@ -1,18 +1,18 @@
-// import mongoose, {Schema} from "mongoose";
-//
-// const categorySchema = new Schema({
-//     title: {
-//         type: String,
-//         required: true,
-//     },
-//     subtitle: {
-//         type: String,
-//         required: true,
-//     },
-//     description: {
-//         type: String,
-//         required: true,
-//     },
-//
-// });
-// export const CategoryModel = mongoose.model('CategoryModel', categorySchema);
+import mongoose,{Schema} from "mongoose";
+
+const categorySchema = new Schema({
+    title:{
+        required: true,
+        type: String
+    },
+    description:{
+        required: false,
+        type: String
+    },
+    thumbnail:{
+        required: true,
+        type: String
+    },
+   
+});
+export const Category = mongoose.models.Category ?? mongoose.model("Category",categorySchema);
--- queires /courses.js
+++ queires /courses.js
@@ -1,9 +1,19 @@
+
+import { Category } from "../model/category-model.js";
 import { Course } from "../model/course-model.js";
+import { User } from "../model/user-model.js";
 import connectToDB from "../service/mongo.js";
 
 export async function getCourses() {
     await connectToDB();
-    const courses = await Course.find({});
+    const courses = await Course.find({}).populate({
+        path : "category",
+        model: Category,
+    }).populate({
+        path: "instructor",
+        model: User,
+    });
+    
     // console.log(courses);
     return courses;
 }
