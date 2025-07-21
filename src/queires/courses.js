6816c22d6cfe33881ad950db80f2ea078c1ade19
Vichea69 <smart.vichea@gmail.com>
2025-07-21 16:45:29 +0700

    Refactor course and user models; implement mongoose schemas and update course queries

---
+++ src/queires/courses.js
@@ -0,0 +1,23 @@
+import { Category } from "@/model/category-model";
+import { Course } from "@/model/course-model";
+import { Module } from "@/model/module.model";
+import { Testimonial } from "@/model/testimonial-model";
+import { User } from "@/model/user-model";
+import { replaceMongoIdInArray } from "@/lib/convertData";
+
+export async function getCourseList() {
+    const courses = await Course.find({}).select(["title","subtitle","thumbnail","modules","price","category","instructor"]).populate({
+        path: "category",
+        model: Category
+    }).populate({
+        path: "instructor",
+        model: User
+    }).populate({
+        path: "testimonials",
+        model: Testimonial
+    }).populate({
+        path: "modules",
+        model: Module
+    }).lean();
+    return replaceMongoIdInArray(courses);
+}  
