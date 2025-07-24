ddab34e209f9b2cac85a27bb34997507b340c586
Vichea69 <smart.vichea@gmail.com>
2025-07-24 13:26:50 +0700

    Add Stripe integration for course enrollment and success handling; implement checkout session creation and format amount utility.

--- src/components/enroll-course.jsx
+++ src/components/enroll-course.jsx
@@ -1,13 +1,20 @@
+'use client';
 import React from 'react';
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import { cn } from '@/lib/utils';
+import { createCheckoutSession } from '@/app/actions/stripe';
 
 const EnrollCourse = ({ asLink }) => {
 
+    const formAction = async(data) => {
+        const { url } = await createCheckoutSession(data);
+        window.location.assign(url);
+    }
+
     return (
  <>
-    <form>
+    <form action={formAction} >
         {asLink ? (
              <Button
              type="submit"
