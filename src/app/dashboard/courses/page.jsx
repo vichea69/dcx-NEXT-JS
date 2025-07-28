0c72a4158c035024ad44febff4ee0873204d5e35
Vichea69 <smart.vichea@gmail.com>
2025-07-28 11:08:04 +0700

    Update course columns and data mapping
    Renamed 'isPublished' to 'active' in columns.

--- src/app/dashboard/courses/page.jsx
+++ src/app/dashboard/courses/page.jsx
@@ -2,31 +2,17 @@ import { getInstructorDashboardData } from "@/lib/dashboard-helper";
 import { columns } from "./_components/columns";
 import { DataTable } from "./_components/data-table";
 import { ObjectId } from "mongoose";
-// const courses = [
-//   {
-//     id: 1,
-//     title: "Reactive Accelerator",
-//     price: 49,
-//     isPublished: true,
-//   },
-//   {
-//     id: 2,
-//     title: "Think In A Redux Way",
-//     price: 10,
-//     isPublished: false,
-//   },
-// ];
+
 const CoursesPage = async () => {
+  const data = sanitizeData(await getInstructorDashboardData());
+  console.log(data);
 
   const courses = sanitizeData(await getInstructorDashboardData()) ;
   console.log(courses);
 
   return (
       <div className="p-6">
-        {/* <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link> */}
-        <DataTable columns={columns} data={courses} />
+        <DataTable columns={columns} data={data?.courses} />
       </div>
   );
 };
