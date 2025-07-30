ca95c91f811db0978f5f8de2d8a185ce6c083e64
Vichea69 <smart.vichea@gmail.com>
2025-07-30 15:42:39 +0700

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -17,5 +17,5 @@ export default auth((res)=>{
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|assets).*)"],
 }
